859f6e93a3c8ebadc730fcc7935ff365deab134c
karthik20066002 <karthik20066002@gmail.com>
2024-11-04 00:48:15 +0530

    Begun day 1, and set up the tasks.json files.

---
+++ day_1/helloWorld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+//iostream used to print and read from console.
+
+int main()
+{
+                                    //remember, main is what contains the body, so kinda like __main__ in python.
+	std::cout << "Hello, world!";   //std::cout is in the iostream library
+	return 0;
+}
