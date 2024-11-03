6f650a223b033961c6d0cae11befe6ea8d00c0a0
karthik20066002 <karthik20066002@gmail.com>
2024-11-04 01:15:08 +0530

    Begun day 1, and set up the tasks.json files.

--- day_1/helloWorld.cpp
+++ day_1/helloWorld.cpp
@@ -6,4 +6,6 @@ int main()
                                     //remember, main is what contains the body, so kinda like __main__ in python.
 	std::cout << "Hello, world!";   //std::cout is in the iostream library
 	return 0;
+
+    int x,y;
 }
